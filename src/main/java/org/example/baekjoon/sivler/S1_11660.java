6ed7af15a9c9fc5bdd4fa89428d3d91d5ae32c34
birdie <flaehdan@gmail.com>
2024-07-26 15:16:37 +0900

    백준

---
+++ src/main/java/org/example/baekjoon/sivler/S1_11660.java
@@ -0,0 +1,51 @@
+package org.example.baekjoon.sivler;
+
+import java.io.*;
+import java.util.*;
+
+public class S1_11660 {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] dp = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] += dp[i][j - 1] + arr[i][j];
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int answer = 0;
+
+            for (int j = x1; j <= x2; j++) {
+                answer += dp[j][y2] - dp[j][y1 - 1];
+            }
+
+            bw.write(answer + "\n");
+        }
+
+        bw.flush();
+    }
+}
